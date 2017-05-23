e6b4eb238467239160a5b053661497c525ce1fac
Siu Soan <siusoandesign@gmail.com>
2017-05-23 15:45:27 +0200

    Prototyping vragenlijst.html
    1. Vragenlijst.html
    HTML-structuur verbeteren en aanvullen
    Styling verbeteren en aanvullen
    
    2. Afbeeldingen toegevoegd
    Voorlopige icon en (definitieve) profile afbeeldingen reeds in gebruik
    gemaakt
    
    3. Index.js
    Wilde al iets met Javascript doen, maar eerst window.onload function
    toegevoegd.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass("hamburger");
-        $("#wrapper").toggleClass("menuDisplayed");
-    });
-});
+function main() {
+  $(document).ready(function() {
+      $("#menu-toggle").click(function(e) {
+          e.preventDefault();
+          $(this).toggleClass("hamburger");
+          $("#wrapper").toggleClass("menuDisplayed");
+      });
+  });
 
-/*// Om .les dezelfde hoogte als breedte te geven
-var cw = $('.les').width();
-$('.les').css({'height':cw+'px'});*/
+  /*// Om .les dezelfde hoogte als breedte te geven
+  var cw = $('.les').width();
+  $('.les').css({'height':cw+'px'});*/
+
+  
+}
+
+
+
+window.onload = function() {
+  main();
+}
