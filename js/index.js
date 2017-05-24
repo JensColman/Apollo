1fe0cf92293554004cb08c29947db63d14f7262a
Siu Soan <siusoandesign@gmail.com>
2017-05-24 14:21:25 +0200

    Werkende Quill.js implementatie
    Werkende Quill.js met toggle functie

--- js/index.js
+++ js/index.js
@@ -11,20 +11,45 @@ function main() {
   var cw = $('.les').width();
   $('.les').css({'height':cw+'px'});*/
 
-  /*var editor = document.getElementById("editor");
+
+  //Quill.js implementatie in vraag.html
+  var editor = document.getElementById("editor");
   var quill = new Quill('#editor', {
         theme: 'snow',   // Specify theme in configuration
         modules: {
-            toolbar: [
-                [{ 'font': [] }],
-                [{ 'align': [] }],
+            toolbar: [  // custom dropdown
+                [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
                 ['bold', 'italic'],
                 ['link', 'blockquote', 'image'],
                 [{ list: 'ordered' }, { list: 'bullet' }]
             ]
         }
   });
-  quill.setText('Schrijf een antwoord...');*/
+  quill.setText('Schrijf een antwoord...');
+
+
+  //Toggle functionaliteit voor Reacties en Beantwoorden knoppen in vraag.html
+  var reactiesButton = document.getElementById("vraag-reactie"), reactiesBeantwoord = document.getElementById("vraag-beantwoord"), antwoorden = document.getElementById("sectie-antwoorden"), beantwoorden = document.getElementById("sectie-beantwoorden");
+
+  function toggleAntwoorden() {
+      if (antwoorden.className === "content-vraag-sectie antwoorden onzichtbaar") {
+          antwoorden.className = "content-vraag-sectie antwoorden";
+          beantwoorden.className = "tool-wrapper onzichtbaar";
+      } else {
+          antwoorden.className = "content-vraag-sectie antwoorden onzichtbaar";
+      }
+  }
+
+  function toggleToolbar() {
+
+      if (beantwoorden.className === "tool-wrapper onzichtbaar") {
+          beantwoorden.className = "tool-wrapper";
+      } else {
+          beantwoorden.className = "tool-wrapper onzichtbaar";
+      }
+  }
+
+  reactiesBeantwoord.addEventListener('click', toggleToolbar, false)
 
 }
 
